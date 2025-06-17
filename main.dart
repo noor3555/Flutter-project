f629e5212aabb788f78e2fa8bf27a339a245a68d
noor3555 <152720879+noor3555@users.noreply.github.com>
2025-06-17 14:20:12 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'firebase_options.dart';
+import 'Manager_Home_Page.dart';
+//import 'add_student.dart';
+//import 'add_teacher.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:ManagerHomePage() ,
+    );
+  }
+}
