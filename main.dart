f5e5185c8486ec029ddb6b818fdbc504ad8a76dc
noor3555 <152720879+noor3555@users.noreply.github.com>
2025-06-21 14:29:23 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'firebase_options.dart';
-import 'Manager_Home_Page.dart';
-//import 'add_student.dart';
-//import 'add_teacher.dart';
+import 'warpper.dart';
+import 'package:get/get.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -12,14 +11,15 @@ void main() async {
   );
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      title: 'مدرستي الالكترونية',
       debugShowCheckedModeBanner: false,
-      home:ManagerHomePage() ,
+      home: Wrapper(),
     );
   }
 }
